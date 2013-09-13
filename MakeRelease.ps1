e1bf201237b299d4f8edcbe686c5676faa20cb79
Jason Shirk <jason@truewheels.net>
2013-09-13 09:16:53 -0700

    New release

--- MakeRelease.ps1
+++ MakeRelease.ps1
@@ -1,4 +1,6 @@
 
+param([switch]$Install)
+
 add-type -AssemblyName System.IO.Compression.FileSystem
 
 if (!(gcm msbuild -ea Ignore))
@@ -16,12 +18,51 @@ if (Test-Path $targetDir)
 }
 
 $null = mkdir $targetDir 
+$null = mkdir $targetDir\en-US
 
-cp $PSScriptRoot\PSReadline\bin\Release\PSReadline.dll $targetDir
-cp -re $PSScriptRoot\PSReadline\en-US $targetDir
-cp $PSScriptRoot\PSReadline\PSReadline.psd1 $targetDir
-cp $PSScriptRoot\PSReadline\PSReadline.psm1 $targetDir
-cp $PSScriptRoot\PSReadline\PSReadline.format.ps1xml $targetDir
+$files = @('Changes.txt',
+           'License.txt',
+           'PSReadline\PSReadline.psd1',
+           'PSReadline\PSReadline.psm1',
+           'PSReadline\PSReadline.format.ps1xml',
+           'PSReadline\PSReadline.format.ps1xml',
+           'PSReadline\bin\Release\PSReadline.dll')
+
+foreach ($file in $files)
+{
+    cp $PSScriptRoot\$file $targetDir
+}
+
+$files = @('PSReadline\en-US\about_PSReadline.help.txt',
+           'PSReadline\en-US\PSReadline.dll-help.xml')
+
+foreach ($file in $files)
+{
+    cp $PSScriptRoot\$file $targetDir\en-us
+}
 
 del $PSScriptRoot\PSReadline.zip -ea Ignore
 [System.IO.Compression.ZipFile]::CreateFromDirectory($targetDir, "$PSScriptRoot\PSReadline.zip")
+
+if ($Install)
+{
+    $InstallDir = "${env:HOME}\Documents\WIndowsPowerShell\Modules"
+
+    if (!(Test-Path $InstallDir))
+    {
+        mkdir -force $InstallDir
+    }
+
+    try
+    {
+        if (Test-Path $InstallDir\PSReadline)
+        {
+            rm -Recurse -force $InstallDir\PSReadline -ea Stop
+        }
+        cp -Recurse $targetDir $InstallDir
+    }
+    catch
+    {
+        Write-Error "Can't install, module is probably in use."
+    }
+}
