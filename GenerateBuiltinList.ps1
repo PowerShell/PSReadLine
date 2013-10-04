e877cd2c81529d0608140465184b5c17f7c00ffc
Jason Shirk <jason@truewheels.net>
2013-10-03 23:17:02 -0700

    Simplify adding handlers to builtin functions

---
+++ GenerateBuiltinList.ps1
@@ -0,0 +1,47 @@
+
+# Generate the ValidateSet attribute for the Builtin parameter
+# Set-PSReadlineKeyHandler
+
+$methods = ([PSConsoleUtilities.PSConsoleReadLine] |
+    Get-Member -Static -Type Method |
+    Where-Object Definition -match .*Nullable.*).Name
+
+$indent = " " * 8
+$prefix = "[ValidateSet("
+$suffix = "]"
+
+function GenerateStrings
+{
+    param([Parameter(ValueFromPipeline)]$Item)
+
+    begin
+    {
+        $count = 0
+        $comma = ""
+        $buffer = New-Object System.Text.StringBuilder
+    }
+
+    process
+    {
+        $null = $buffer.Append($comma)
+        $count++
+        if (($count % 5) -eq 0)
+        {
+            # Remove the trailing space
+            $null = $buffer.Remove($buffer.Length - 1, 1)
+            $null = $buffer.Append("`n$indent$(' ' * $prefix.Length)")
+        }
+        $null = $buffer.Append('"')
+        $null = $buffer.Append($Item)
+        $null = $buffer.Append('"')
+
+        $comma = ", "
+    }
+
+    end
+    {
+        $buffer.ToString()
+    }
+}
+
+"$indent$prefix$($methods | GenerateStrings))$suffix"
