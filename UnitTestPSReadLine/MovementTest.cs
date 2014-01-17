3e65239c20e9da6c7fcfc07c9076319ef49e9ab5
jason@truewheels.net <jason@truewheels.net>
2014-01-16 16:51:40 -0800

    GotoBrace tests

--- UnitTestPSReadLine/MovementTest.cs
+++ UnitTestPSReadLine/MovementTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PSConsoleUtilities;
 
@@ -58,6 +59,52 @@ namespace UnitTestPSReadLine
                 CheckThat(() => AssertCursorLeftIs(1)),
                 'b'
                 ));
+        }
+
+        [TestMethod]
+        public void TestGotoBrace()
+        {
+            TestSetup(KeyMode.Cmd);
+
+            // Test empty input
+            Test("", Keys(_.CtrlRBracket));
+
+            Test("(11)", Keys("(11)", _.LeftArrow, _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(0))));
+            Test("$a[11]", Keys("$a[11]", _.LeftArrow, _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(2))));
+            Test("{11}", Keys("{11}", _.LeftArrow, _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(0))));
+            Test("(11)", Keys("(11)", _.Home, _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(3))));
+            Test("$a[11]", Keys("$a[11]", _.Home, _.RightArrow, _.RightArrow, _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(5))));
+            Test("{11}", Keys("{11}", _.Home, _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(3))));
+
+            // Test multiples, make sure we go to the right one.
+            Test("((11))", Keys("((11))", _.LeftArrow, _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(0))));
+            Test("((11))", Keys("((11))", _.LeftArrow, _.LeftArrow, _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(1))));
+
+            using (ShimsContext.Create())
+            {
+                bool ding = false;
+                PSConsoleUtilities.Fakes.ShimPSConsoleReadLine.Ding =
+                    () => ding = true;
+
+                // Make sure we don't match inside a string
+                Test("", Keys(
+                    "'()'", _.LeftArrow, _.LeftArrow,
+                    _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(2)),
+                    _.CtrlC, InputAcceptedNow));
+                Assert.IsTrue(ding);
+
+                foreach (var c in new[] {'(', ')', '{', '}', '[', ']'})
+                {
+                    ding = false;
+                    PSConsoleUtilities.Fakes.ShimPSConsoleReadLine.Ding =
+                        () => ding = true;
+                    Test("", Keys(
+                        'a', c, _.LeftArrow,
+                        _.CtrlRBracket, CheckThat(() => AssertCursorLeftIs(1)),
+                        _.CtrlC, InputAcceptedNow));
+                    Assert.IsTrue(ding);
+                }
+            }
         }
     }
 }
