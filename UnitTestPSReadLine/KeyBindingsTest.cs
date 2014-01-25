4a6d0591a550ec6d41b122ef1297d1203c2f8f69
jason@truewheels.net <jason@truewheels.net>
2014-01-24 21:29:40 -0800

    WhatIsKey tests

---
+++ UnitTestPSReadLine/KeyBindingsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSConsoleUtilities;
+
+namespace UnitTestPSReadLine
+{
+    // Disgusting language hack to make it easier to read a sequence of keys.
+    using _ = Keys;
+
+    public partial class UnitTest
+    {
+        [TestMethod]
+        public void TestWhatIsKey()
+        {
+            TestSetup(KeyMode.Cmd);
+
+            Test("", Keys(
+                _.AltQuestion,
+                CheckThat(() => AssertScreenIs(2, NextLine, "what-is-key:")),
+                'a',
+                CheckThat(() => AssertScreenIs(2, NextLine, "a: SelfInsert - Insert the key typed"))));
+
+            Test("", Keys(
+                _.AltQuestion, _.F24,
+                CheckThat(() => AssertScreenIs(2, NextLine, "F24: Key is unbound"))));
+
+            Test("", Keys(
+                _.AltQuestion, _.LeftArrow,
+                CheckThat(() => AssertScreenIs(2, NextLine, "LeftArrow: BackwardChar - Move the cursor back one character"))
+                ));
+
+            TestSetup(KeyMode.Emacs);
+            Test("", Keys(
+                _.AltQuestion, _.CtrlX, _.CtrlU,
+                CheckThat(() => AssertScreenIs(2, NextLine, "Ctrl+x,Ctrl+u: Undo - Undo a previous edit"))));
+        }
+    }
+}
