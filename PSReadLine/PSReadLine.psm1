e5371f5ec52119e4e0eb475e1a1402ce03351c46
Dan Thompson <dan_j_thompson@hotmail.com>
2014-02-22 22:32:21 -0800

    Replace static host check in .psd1 with runtime host check in .psm1.
    This will allow other hosts to be added to the list of hosts that support
    PSReadline. (The PowerShellHost member of the .psd1 can only specify a
    single host name.)

--- PSReadLine/PSReadLine.psm1
+++ PSReadLine/PSReadLine.psm1
@@ -1,4 +1,16 @@
 
+if ($Host.Name -notin 'ConsoleHost', 'ColorConsoleHost')
+{
+    # This is the sort of error you would get if you were using the 'PowerShellHost' field
+    # in the .psd1 to enforce a certain host name:
+    #
+    #    Import-Module : The name of the current Windows PowerShell host is: 'ColorConsoleHost'. The module
+    #    'C:\Users\me\Documents\WindowsPowerShell\Modules\psreadline\psreadline.psd1' requires the following Windows
+    #    PowerShell host: 'ConsoleHost'.
+    #
+    throw "The name of the current Windows PowerShell host is: '$($Host.Name)'. The PSReadline module does not support this host."
+}
+
 #
 # .SYNOPSIS
 #
@@ -8,6 +20,6 @@ function PSConsoleHostReadline
 {
     [PSConsoleUtilities.PSConsoleReadLine]::ReadLine()
 }
- 
+
 # Load history
 Get-History | ForEach-Object { [PSConsoleUtilities.PSConsoleReadLine]::AddToHistory($_.CommandLine) }
