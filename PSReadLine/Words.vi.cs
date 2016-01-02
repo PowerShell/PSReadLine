b80ffb9b091af0455d95c837ebcebea2e68dfe4f
srdubya <stevewy@gmail.com>
2016-01-02 08:51:06 -0700

    Addressed this feedback: 1  _normalBackground (keybindings.vi.cs) unused 2  copyright notice in *.vi.cs files? 3  ReadLine.vi.cs - InsertCharacter unreferenced 4  SearchBackward - change fn name to ViSearchHistoryBackward?

--- PSReadLine/Words.vi.cs
+++ PSReadLine/Words.vi.cs
@@ -1,3 +1,7 @@
+/********************************************************************++
+Copyright (c) Microsoft Corporation.  All rights reserved.
+--********************************************************************/
+
 using System.Collections.Generic;
 using System.Management.Automation.Language;
 
