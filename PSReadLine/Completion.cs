0b7a189536188e2f1f253c538a6ca09ac0078364
Jason Shirk <jasonsh@microsoft.com>
2017-11-04 10:43:34 -0700

    Filter out apparent duplicates during tab completion
    Fix #413

--- PSReadLine/Completion.cs
+++ PSReadLine/Completion.cs
@@ -306,6 +306,20 @@ namespace Microsoft.PowerShell
                     var length = _tabCompletions.ReplacementLength;
                     if (start < 0 || start > _singleton._buffer.Length) return null;
                     if (length <= 0 || length > (_singleton._buffer.Length - start)) return null;
+
+                    if (_tabCompletions.CompletionMatches.Count > 1)
+                    {
+                        // Filter out apparent duplicates
+                        var hashSet = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+                        foreach (var match in _tabCompletions.CompletionMatches.ToArray())
+                        {
+                            if (!hashSet.Add(match.ListItemText))
+                            {
+                                _tabCompletions.CompletionMatches.Remove(match);
+                            }
+                        }
+                    }
                 }
                 catch (Exception)
                 {
