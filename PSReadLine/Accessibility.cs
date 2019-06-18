f199fb1eda7fc4e9034378629ffd76430ad28d9c
Steve Lee (POWERSHELL) <slee@microsoft.com>
2019-06-18 13:14:48 -0700

    add API to detect screenreader

---
+++ PSReadLine/Accessibility.cs
@@ -0,0 +1,23 @@
+/********************************************************************++
+Copyright (c) Microsoft Corporation.  All rights reserved.
+--********************************************************************/
+
+using System.Runtime.InteropServices;
+
+namespace Microsoft.PowerShell.Internal
+{
+    internal class Accessibility
+    {
+        internal static bool IsScreenReaderActive()
+        {
+            bool returnValue = false;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                PlatformWindows.SystemParametersInfo(PlatformWindows.SPI_GETSCREENREADER, 0, ref returnValue, 0);
+            }
+
+            return returnValue;
+        }
+    }
+}
