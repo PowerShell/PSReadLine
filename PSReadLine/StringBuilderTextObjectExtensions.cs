c6fb8e0785f2b0ba1321b3389f568c0610867827
Dongbo Wang <dongbow@microsoft.com>
2023-08-14 15:44:42 -0700

    Revert some changes that caused test failure

--- PSReadLine/StringBuilderTextObjectExtensions.cs
+++ PSReadLine/StringBuilderTextObjectExtensions.cs
@@ -5,6 +5,8 @@ namespace Microsoft.PowerShell
 {
     internal static class StringBuilderTextObjectExtensions
     {
+        private const string WhiteSpace = " \n\t";
+
         /// <summary>
         /// Returns the position of the beginning of the current word as delimited by white space and delimiters
         /// This method differs from <see cref="ViFindPreviousWordPoint(string)"/>:
@@ -18,20 +20,34 @@ namespace Microsoft.PowerShell
             // Cursor may be past the end of the buffer when calling this method
             // this may happen if the cursor is at the beginning of a new line.
             var i = Math.Min(position, buffer.Length - 1);
-            Func<char, bool> isTextObjectChar;
 
-            if (buffer.InWord(i, wordDelimiters))
+            // If starting on a word consider a text object as a sequence of characters excluding the delimiters,
+            // otherwise, consider a word as a sequence of delimiters.
+            var delimiters = wordDelimiters;
+            var isInWord = buffer.InWord(i, wordDelimiters);
+
+            if (isInWord)
             {
-                // If starting on a word consider a text object as a sequence of characters excluding the delimiters.
-                isTextObjectChar = c => Character.IsInWord(c, wordDelimiters);
+                // For the purpose of this method, whitespace character is considered a delimiter.
+                delimiters += WhiteSpace;
             }
             else
             {
-                // Otherwise, consider a word as a sequence of delimiters.
-                // For the purpose of this method, a newline (\n), tab (\t), or space is considered delimiter.
-                var delimiters = wordDelimiters + " \n\t";
-                isTextObjectChar = c => delimiters.IndexOf(c) != -1;
+                char c = buffer[i];
+                if ((wordDelimiters + '\n').IndexOf(c) == -1 && char.IsWhiteSpace(c))
+                {
+                    // Current position points to a whitespace that is not a newline.
+                    delimiters = WhiteSpace;
+                }
+                else
+                {
+                    delimiters += '\n';
+                }
             }
+
+            var isTextObjectChar = isInWord
+                ? (Func<char, bool>)(c => delimiters.IndexOf(c) == -1)
+                : c => delimiters.IndexOf(c) != -1;
 
             var beginning = i;
             while (i >= 0 && isTextObjectChar(buffer[i]))
@@ -53,7 +69,6 @@ namespace Microsoft.PowerShell
             // Cursor may be past the end of the buffer when calling this method
             // this may happen if the cursor is at the beginning of a new line.
             var i = Math.Min(position, buffer.Length - 1);
-            Func<char, bool> isTextObjectChar;
 
             // Always skip the first newline character.
             if (buffer[i] == '\n' && i < buffer.Length - 1)
@@ -61,18 +76,23 @@ namespace Microsoft.PowerShell
                 ++i;
             }
 
-            if (buffer.InWord(i, wordDelimiters))
+            // If starting on a word consider a text object as a sequence of characters excluding the delimiters,
+            // otherwise, consider a word as a sequence of delimiters.
+            var delimiters = wordDelimiters;
+            var isInWord = buffer.InWord(i, wordDelimiters);
+
+            if (isInWord)
             {
-                // If starting on a word consider a text object as a sequence of characters excluding the delimiters.
-                isTextObjectChar = c => Character.IsInWord(c, wordDelimiters);
+                delimiters += WhiteSpace;
             }
-            else
+            else if (char.IsWhiteSpace(buffer[i]))
             {
-                // Otherwise, consider a word as a sequence of delimiters.
-                // For the purpose of this method, a newline (\n), tab (\t), or space is considered delimiter.
-                var delimiters = wordDelimiters + " \n\t";
-                isTextObjectChar = c => delimiters.IndexOf(c) != -1;
+                delimiters = " \t";
             }
+
+            var isTextObjectChar = isInWord
+                ? (Func<char, bool>)(c => delimiters.IndexOf(c) == -1)
+                : c => delimiters.IndexOf(c) != -1;
 
             // Try to skip a second newline characters to replicate vim behaviour.
             if (buffer[i] == '\n' && i < buffer.Length - 1)
