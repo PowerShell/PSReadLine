9c1f75d00e71a05bdee8f0a672028e4d9946eb96
Justin Chung <124807742+jshigetomi@users.noreply.github.com>
2026-01-08 13:32:08 -0600

    Remove error message from psm1

--- PSReadLine/PSReadLine.psm1
+++ PSReadLine/PSReadLine.psm1
@@ -1,17 +1,3 @@
-# Check PowerShell version compatibility
-if ($PSVersionTable.PSVersion -lt [Version]'7.4.0') {
-    $errorMessage = @"
-PSReadLine 3.0+ requires PowerShell 7.4 or later.
-Current version: $($PSVersionTable.PSVersion)
-
-To use PSReadLine with your PowerShell version, install an older version:
-  Install-Module PSReadLine -RequiredVersion 2.4.5 -Force -SkipPublisherCheck
-
-To upgrade PowerShell: https://aka.ms/install-powershell
-"@
-    throw $errorMessage
-}
-
 function PSConsoleHostReadLine
 {
     [System.Diagnostics.DebuggerHidden()]
